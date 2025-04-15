331304f9f0c8d52d0c48c10dc567e14808c49192
Vign <113303279+7Vign7@users.noreply.github.com>
2025-04-15 14:05:20 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx';
 import {BrowserRouter} from "react-router-dom";
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename='/yadro-test'>
+  <BrowserRouter basename='/post-editor'>
     <App/>
   </BrowserRouter>
 )
